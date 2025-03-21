791768a4c22bc415e051afbe17bfb8c323240202
chrisbrinchlarsen <chrisbrinchlarsen@gmail.com>
2025-03-21 14:17:46 +0100

    Fixed bugs and added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Instructions
+Run the archinstall script and select your preferred settings. When you get in, you need to create a folder called "documents", and inside it a folder called "repos". At this point you just need to git clone "https://github.com/chrisbrinchlarsen/zuparch.git" while inside the repos folder. CD into the repos folder and run ./zuparch.sh
+
+# Post Installation
+Once the install script is done, you need to manually edit the .xinitrc file to launch whatever programs you want on launch, as well as start your preferred desktop environment. (DWM is already configured)
--- zuparch.sh
+++ zuparch.sh
@@ -56,9 +56,12 @@ cd ../SLSTATUS && sudo make clean install
 
 echo "Finalizing..."
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
-cp ~/documents/repos/zuparch/.bash ~/.bash
-
+cp -r ~/documents/repos/zuparch/.bash ~/
+. .bash/bash_prompt
+. .bash/bash_env
+. .bash/bash_aliases
+. .bash/bash_utils
 echo "systemctl --user enable opentabletdriver.service --now" >> ~/.bashrc
 . ~/.bashrc
 
-echo "You gotta take care of editing .xinitrc and enabling pacman color in /etc/pacman.conf, as well as creating an SSH key for github.
+echo "You gotta take care of editing .xinitrc and enabling pacman color in /etc/pacman.conf, as well as creating an SSH key for github."
